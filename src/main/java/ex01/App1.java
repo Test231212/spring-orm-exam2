be169780a40b3ae694f1548472e6f750da6633c1
test231212 <j910219@gmail.com>
2024-02-15 17:38:46 +0900

    ex01 완료

--- src/main/java/ex01/App1.java
+++ src/main/java/ex01/App1.java
@@ -8,6 +8,15 @@ public class App1 {
         DBData1 dbData = new DBData1(1, "제목1", "내용1", 3, "love", "love@nate.com");
 
         // 2. dbData1를 ViewData1에 옮기시오 - ORM
+        ViewData1 v1 = new ViewData1();
+
+        v1.setBoardId(dbData.getBoardId());
+        v1.setTitle(dbData.getTitle());
+        v1.setContent(dbData.getContent());
+
+        User user = new User(dbData.getUserId(), dbData.getUsername(), dbData.getEmail());
+
+        v1.setUser(user);
 
 
     }
--- src/main/java/ex01/ViewData1.java
+++ src/main/java/ex01/ViewData1.java
@@ -12,6 +12,12 @@ public class ViewData1 {
     private int boardId;
     private String title;
     private String content;
-
     private User user;
+
+    public ViewData1(DBData1 dbData) {
+        this.boardId = dbData.getBoardId();
+        this.title = dbData.getTitle();
+        this.content = dbData.getContent();
+        this.user = new User(dbData.getUserId(), dbData.getUsername(), dbData.getEmail());
+    }
 }
